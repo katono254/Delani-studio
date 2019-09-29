94c39734095712c530ffbb0f943090ce9c76c6ae
jakesIII <jayruma@yahoo.com>
2019-09-29 21:03:44 +0300

    Delete styles in the css/style/sheet

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".clickable1").click(function(){
+        $(".design-hidden").toggle();
+        $(".design-showing").toggle();
+    })
+})
+$(document).ready(function() {
+    $(".clickable2").click(function() {
+      $(".development-hidden").toggle();
+      $(".development-showing").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".clickable3").click(function() {
+      $(".product-hidden").toggle();
+      $(".product-showing").toggle();
+    });
+  });
+  
